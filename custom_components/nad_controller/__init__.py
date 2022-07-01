d928a914a0d5105122b644ed7e70b76e7ef4a1d4
Breina <brecht.derwael@gmail.com>
2022-07-01 19:17:33 +0200

    More logging

--- custom_components/nad_controller/__init__.py
+++ custom_components/nad_controller/__init__.py
@@ -23,6 +23,7 @@ DATA_SCHEMA = vol.Schema({
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    _LOGGER.info("Entering async_setup_entry")
     """Set up NAD Cl 16-60 from a config entry."""
     hass.data.setdefault(DOMAIN, {})
 
@@ -42,12 +43,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         UNDO_UPDATE_LISTENER: undo_listener
     }
 
+    _LOGGER.info("Going to async_setup_platforms")
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    _LOGGER.info("Entering async_unload_entry")
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(
         config_entry, PLATFORMS
@@ -62,5 +65,6 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> 
 
 
 async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    _LOGGER.info("Entering update_listener")
     """Handle options update."""
     await hass.config_entries.async_reload(config_entry.entry_id)
